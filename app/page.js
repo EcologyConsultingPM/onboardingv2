ab240e738cdb9a1ba274795275a0c8557412dbf8
EcologyConsultingPM <aaron.dooley@ecologyconsulting.au>
2026-08-17 11:56:25 +1000

    Delete app directory

--- app/page.js
+++
@@ -1,9 +0,0 @@
-import OnboardingWorkbook from "../components/OnboardingWorkbook";
-
-export default function Home() {
-  return (
-    <main style={{ height: "100vh", padding: 16 }}>
-      <OnboardingWorkbook />
-    </main>
-  );
-}
